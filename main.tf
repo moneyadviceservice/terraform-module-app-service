a712e129c75b8167a66891f6cdb3aa1d40906539
Zoe Cope <zoe.cope@hmcts.net>
2024-10-15 14:40:24 +0100

    add variable descriptions

--- main.tf
+++ main.tf
@@ -7,23 +7,35 @@ resource "azurerm_service_plan" "this" {
 }
 
 resource "azurerm_linux_web_app" "this" {
+  count               = var.os_type == "Linux" ? 1 : 0
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = azurerm_service_plan.this.location
   service_plan_id     = azurerm_service_plan.this.id
-
+  app_settings        = var.app_settings
+  tags                = var.tags
+  identity {
+    type = SystemAssigned
+  }
   site_config {
-
+    application_stack {}
   }
 }
 
 resource "azurerm_windows_web_app" "this" {
+  count               = var.os_type == "Windows" ? 1 : 0
   name                = var.name
   resource_group_name = var.resource_group_name
   location            = azurerm_service_plan.this.location
   service_plan_id     = azurerm_service_plan.this.id
-
-  site_config {}
+  app_settings        = var.app_settings
+  tags                = var.tags
+  identity {
+    type = SystemAssigned
+  }
+  site_config {
+    application_stack {}
+  }
 }
 
 module "application_insights" {
--- variables.tf
+++ variables.tf
@@ -1,27 +1,75 @@
-variable "name" {
-
-}
-
-variable "product" {
-
-}
-
 variable "resource_group_name" {
+  type        = string
+  description = "The resource group your function app will be assigned to"
+}
 
+variable "name" {
+  type        = string
+  description = "The name of your function app"
 }
 
 variable "location" {
-
-}
-
-variable "os_type" {
-
-}
-
-variable "sku_name" {
-
+  type    = string
+  default = "UK South"
 }
 
 variable "env" {
-  
+  type        = string
+  description = "The environment to deploy to"
+}
+
+variable "app_settings" {
+  type        = map(string)
+  description = "(Optional) A map of key-value pairs for App Settings and custom values."
+}
+
+variable "os_type" {
+  type        = string
+  description = "(Required) The O/S type for the App Services to be hosted in this plan."
+  default     = "Linux"
+}
+
+variable "product" {
+  type        = string
+  description = "The product name"
+}
+
+variable "create_service_plan" {
+  type        = bool
+  description = "If true a new service plan is created"
+  default     = true
+}
+
+variable "service_plan_id" {
+  type        = string
+  description = "If you want to use an existing service plan"
+  default     = null
+}
+variable "sa_account_tier" {
+  type        = string
+  description = "(Required) Defines the Tier to use for this storage account"
+  default     = "Standard"
+}
+
+variable "account_replication_type" {
+  type        = string
+  description = "(Required) Defines the type of replication to use for this storage account."
+  default     = "ZRS"
+}
+
+variable "account_kind" {
+  type        = string
+  description = "(Optional) Defines the Kind of account"
+  default     = "StorageV2"
+}
+
+variable "sku_name" {
+  type        = string
+  description = "(Required) The SKU for the plan."
+  default     = "Y1"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "A mapping of tags assigned to the Resource."
 }
