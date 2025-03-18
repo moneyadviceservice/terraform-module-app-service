ca9f6b3b258a630a31adf24b4641c52df3527bef
Zoe Cope <zoe.cope@hmcts.net>
2025-03-18 11:52:33 +0000

    don't count

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,8 +1,8 @@
 resource "azurerm_monitor_autoscale_setting" "scale_out" {
-  name                = azurerm_service_plan.this[count.index].name
+  name                = azurerm_service_plan.this.name
   resource_group_name = var.resource_group_name
   location            = var.location
-  target_resource_id  = azurerm_service_plan.this[count.index].id
+  target_resource_id  = azurerm_service_plan.this.id
 
   profile {
     name = "Scale out condition"
@@ -35,7 +35,7 @@ resource "azurerm_monitor_autoscale_setting" "scale_out" {
     rule {
       metric_trigger {
         metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.this[count.index].id
+        metric_resource_id = azurerm_service_plan.this.id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
