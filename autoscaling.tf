dedb69e8372cb7babb5b6b998da82225cd3308b5
Zoe Cope <zoe.cope@hmcts.net>
2025-03-18 11:58:17 +0000

    dependency

--- autoscaling.tf
+++ autoscaling.tf
@@ -1,8 +1,9 @@
 resource "azurerm_monitor_autoscale_setting" "scale_out" {
-  name                = azurerm_service_plan.this.name
+  count               = var.app_service_plan_id == null ? 1 : 0
+  name                = azurerm_service_plan.this[count.index].name
   resource_group_name = var.resource_group_name
   location            = var.location
-  target_resource_id  = azurerm_service_plan.this.id
+  target_resource_id  = azurerm_service_plan.this[count.index].id
 
   profile {
     name = "Scale out condition"
@@ -35,7 +36,7 @@ resource "azurerm_monitor_autoscale_setting" "scale_out" {
     rule {
       metric_trigger {
         metric_name        = "CpuPercentage"
-        metric_resource_id = azurerm_service_plan.this.id
+        metric_resource_id = azurerm_service_plan.this[count.index].id
         time_grain         = "PT1M"
         statistic          = "Average"
         time_window        = "PT5M"
